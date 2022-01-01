49b891fd57e7d40a45d5c50a36639d6d61ade0a3
Heytemisan <temyicy@gmail.com>
2022-01-01 11:46:55 +0100

    updated App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,12 @@
-import { useState } from 'react'
+import React from 'react'
 
-const App =()=> {
+function App() {
 
   return (
     <div className="App">
-        <h1 className="text-3xl font-bold underline">      
-            Hello world!
-        </h1>
+      <h1 className="text-3xl font-bold underline">  
+          Hello world!   
+      </h1>
     </div>
   )
 }
