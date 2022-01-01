9ed8725513f651b87de2b427b0f33304fbf1a058
Heytemisan <temyicy@gmail.com>
2022-01-01 13:09:41 +0100

    updated Transaction

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React from 'react';
+import {HiMenuAlt4} from 'react-icon/hi';
+import {AiOutlineClose} from 'react-icon/ai';
+import logo from '../../images/logo.png';
 
 const Navbar = () => {
     return (
-        <div>
+        <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             Navbar
-        </div>
+        </nav>
     )
 }
 
