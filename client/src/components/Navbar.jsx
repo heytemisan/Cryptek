8090cb9a90e8849b35fa4b232a4d1f3bc3e29481
Heytemisan <temyicy@gmail.com>
2022-01-01 20:46:35 +0100

    updated Navbar,jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {HiMenuAlt4} from 'react-icon/hi';
-import {AiOutlineClose} from 'react-icon/ai';
+import {HiMenuAlt4} from 'react-icons/hi';
+import {AiOutlineClose} from 'react-icons/ai';
 import logo from '../../images/logo.png';
+
+const NavbarItem = ({title, classProps}) => {
+    return (
+        <li className={`mx-4 cursor-pointer ${}`}>
+
+        </li>
+    )
+}
 
 const Navbar = () => {
     return (
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
-            Navbar
+            <div className="md:flex-[0.5] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+            <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+
+            </ul>
         </nav>
     )
 }
