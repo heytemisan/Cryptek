92348a057ca96fe48270973f07dae7a0619a3584
Heytemisan <temyicy@gmail.com>
2022-01-02 08:40:29 +0100

    updated Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -6,9 +6,9 @@ import logo from '../../images/logo.png';
 const NavbarItem = ({title, classProps}) => {
     return (
         <li className={`mx-4 cursor-pointer ${classProps}`}>
-
+            {title}
         </li>
-    )
+    );
 }
 
 const Navbar = () => {
@@ -17,8 +17,13 @@ const Navbar = () => {
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer" />
             </div>
-            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-
+            <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+                {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index)=> (
+                    <NavbarItem key={item + index} title={item}/>
+                ))}
+                <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full">
+                    Login
+                </li>
             </ul>
         </nav>
     )
