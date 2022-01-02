63b1fbb52b94758360c4966eb2c3d8d2a45e4966
Heytemisan <temyicy@gmail.com>
2022-01-02 08:50:50 +0100

    updated Navbar.jsx

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -12,12 +12,15 @@ const NavbarItem = ({title, classProps}) => {
 }
 
 const Navbar = () => {
+
+    const [toggleMenu, setToggleMenu] = React.useState(false);
+
     return (
         <nav className='w-full flex md:justify-center justify-between items-center p-4'>
             <div className="md:flex-[0.5] flex-initial justify-center items-center">
                 <img src={logo} alt="logo" className="w-32 cursor-pointer" />
             </div>
-            <ul className="text-white md:flex hiddent list-none flex-row justify-between items-center flex-initial">
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
                 {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index)=> (
                     <NavbarItem key={item + index} title={item}/>
                 ))}
@@ -25,6 +28,11 @@ const Navbar = () => {
                     Login
                 </li>
             </ul>
+            <div className="flex-relative">
+                {toggleMenu ? 
+                <AiOutlineClose fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(false)}/> : 
+                <HiMenuAlt4 fontSize={28} className='text-white md:hidden cursor-pointer' onClick={() => setToggleMenu(true)}/>}
+            </div>
         </nav>
     )
 }
