0dc297415f9c55e9f9b6f0554f96c65a0ee4ea63
Heytemisan <temyicy@gmail.com>
2022-01-03 12:33:52 +0100

    updayed Transactions.sol

--- smartContract/scripts/deploy.js
+++ smartContract/scripts/deploy.js
@@ -1,17 +1,17 @@
 const main = async () => {
 
   const Transactions = await hre.ethers.getContractFactory("Transactions");
-  const transaction = await Transactions.deploy();
+  const transactions = await Transactions.deploy();
 
-  await transaction.deployed();
+  await transactions.deployed();
 
-  console.log("Transactions deployed to:", transaction.address);
+  console.log("Transactions deployed to:", transactions.address);
 }
 
 const runMain = async () => {
   try {
     await main();
-    process.exit(0)
+    process.exit(0);
   } catch (error) {
     console.error(error);
     process.exit(1);
