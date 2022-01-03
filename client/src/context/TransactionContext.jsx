632b2ef3cee8322cc29c61eb3def5d083ffa9d04
Heytemisan <temyicy@gmail.com>
2022-01-03 14:39:25 +0100

    updated constants.js

---
+++ client/src/context/TransactionContext.jsx
@@ -0,0 +1,4 @@
+import React , {useState, useEffect} from 'react';
+import { ethers } from 'ethers';
+
+import {contractABI , contractAddress } from "../utils";
---
+++ client/src/utils/constants.js
@@ -0,0 +1,3 @@
+import abi from './Transaction.json'
+export const contractAddress = '0x0289EC06064ADBF7D0F183e0e459DCf8e18a52AD';
+export const contractABI = abi.abi
