f7852a0c0f90a11504a563aef6e1a37e37d71637
Heytemisan <temyicy@gmail.com>
2022-01-03 14:07:06 +0100

    updated Transaction.js

--- smartContract/contracts/Transactions.sol
+++ smartContract/contracts/Transactions.sol
@@ -1,12 +1,14 @@
-//SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8.0; 
+pragma solidity ^0.8.0;
 
-contract Transactions {j
+import "hardhat/console.sol";
+
+contract Transactions {
     uint256 transactionCount;
 
     event Transfer(address from, address receiver, uint amount, string message, uint256 timestamp, string keyword);
-
+  
     struct TransferStruct {
         address sender;
         address receiver;
@@ -18,18 +20,18 @@ contract Transactions {j
 
     TransferStruct[] transactions;
 
-    function addToBlockchain (address payable receiver, uint amount, string memory message, string memory keyword) public {
+    function addToBlockchain(address payable receiver, uint amount, string memory message, string memory keyword) public {
         transactionCount += 1;
-        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword)); 
+        transactions.push(TransferStruct(msg.sender, receiver, amount, message, block.timestamp, keyword));
 
         emit Transfer(msg.sender, receiver, amount, message, block.timestamp, keyword);
     }
 
-    function getAllTransactions () public view returns (TransferStruct [] memory){
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
         return transactions;
     }
 
-    function getTransactionCount () public view returns (uint256) {
+    function getTransactionCount() public view returns (uint256) {
         return transactionCount;
     }
 }
--- smartContract/hardhat.config.js
+++ smartContract/hardhat.config.js
@@ -1,12 +1,13 @@
 // https://eth-ropsten.alchemyapi.io/v2/pVhPL6TPkztNv_sukRxRDR-XV29kff3H
 
 require('@nomiclabs/hardhat-waffle');
+
 module.exports = {
   solidity:'0.8.0',
   networks: {
     ropsten: {
       url: 'https://eth-ropsten.alchemyapi.io/v2/pVhPL6TPkztNv_sukRxRDR-XV29kff3H',
-      account: ['c09521b950aa1a677f368122d57bfed9bbd9dca270006653b5040d392b4499a1'],
+      accounts: ['c09521b950aa1a677f368122d57bfed9bbd9dca270006653b5040d392b4499a1'],
     },
   },
 }
