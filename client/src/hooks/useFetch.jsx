cf2c44d2ac34127391ea15514fff6036b4454fdf
Heytemisan <temyicy@gmail.com>
2022-01-04 21:59:19 +0100

    updated useFetch.jsx

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,15 +1,22 @@
-import {useState, UseState} from 'react';
+import {useState, useEffect} from 'react';
 const API_KEY = import.meta.env.VITE_GIPHY_API;
 
-const useFetch =()=> {
+const useFetch =({keyword})=> {
 
     const [gifUrl, setGifUrl] = useState("")
 
     try {
         const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`);
         const { data } = await response.json();
-        setGifUrl(data[0]?.images?downsized)
+        setGifUrl(data[0]?.images?.downsized_meduim?.url)
     } catch (error) {
-        
+        setGifUrl('https://www.omnisend.com/blog/wp-content/uploads/2016/09/funny-gifs-9.gif')
     }
+
+    useEffect(() => {
+        //if there is a keyword, we will call the fetchGifs method
+        if(keyword) fetchGifs(); 
+        return () => {
+        }
+    }, [keyword])
 }
