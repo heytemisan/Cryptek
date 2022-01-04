6563b4bc85fc2c6cb2becdbce331ea89c7e558f3
Heytemisan <temyicy@gmail.com>
2022-01-04 09:21:01 +0100

    created dummyData.js

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, {useContext} from 'react';
+import {TransactionContext} from '../context/TransactionContext';
+import dummyData from '../utils/dummyData'; 
 
 const Transactions = () => {
     return (
-        <div>
-            Transactions
+        <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
+            <div className="flex flex-col md:p-12 py-12 px-4">
+                
+            </div>
         </div>
     )
 }
