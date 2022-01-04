369ba9cda2891419f5395f8dcc9f5ee88319bc0f
Heytemisan <temyicy@gmail.com>
2022-01-04 11:19:40 +0100

    updated Transaction.jsx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -1,6 +1,33 @@
 import React, {useContext} from 'react';
 import {TransactionContext} from '../context/TransactionContext';
 import dummyData from '../utils/dummyData'; 
+import {shortenAddress} from '../utils/shortenAddress';
+
+const TransactionCard = ({addressTo, addressFrom, timestamp, keyword, amount, url}) => {
+    return (
+        <div className="
+            bg-[#181918] 
+            m-4 flex flex-1
+            2xl:min-w-[450px]
+            2xl:max-w-[500px]
+            sm:min-w-[270px]
+            sm:max-w-[300px]
+            flex-col p-3 rounded-md
+            hover:shadow-2xl
+        ">
+            <div className="flex flex-col items-center w-full mt-3">
+                <div className="display-flex justifu-start w-full mb-6 p-2">
+                    <a href={`https://ropsten.etherscan.io/address/${addressFrom}`} target="_blank" rel="noopener noreferrer">
+                        <p className="text-white text-base">
+                            
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    )
+}
+console.log(TransactionCard);
 
 const Transactions = () => {
     const {currentAccount} = useContext(TransactionContext)
@@ -16,6 +43,14 @@ const Transactions = () => {
                         Connect your account to see the latest transactions
                     </h3>
                 )}
+            </div>
+            <div className="flex flex-wrap justify-center items-center mt-10 text-white">
+                {dummyData.reverse().map((transaction, i)=> (
+                    <TransactionCard 
+                        key={i}
+                        {...transaction}
+                    />
+                ))}
             </div>
         </div>
     )
