460f5a2f9f164223b6b74870aff5e4d79f9299f8
Heytemisan <temyicy@gmail.com>
2022-01-04 08:00:44 +0100

    updated tailwind.confid.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
+// tailwind.config.js
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
