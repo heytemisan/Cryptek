7baa27a8cdf6ca88190e0f382285a4d26991e810
Heytemisan <temyicy@gmail.com>
2022-01-08 10:14:50 +0100

    updated Transaction.jsx

--- client/src/hooks/useFetch.jsx
+++ client/src/hooks/useFetch.jsx
@@ -1,15 +1,15 @@
 import {useState, useEffect} from 'react';
-const API_KEY = import.meta.env.VITE_GIPHY_API;
+const APIKEY = import.meta.env.VITE_GIPHY_API;
 
 const useFetch =({keyword})=> {
 
-    const [gifUrl, setGifUrl] = useState("")
+    const [gifUrl, setGifUrl] = useState("");
 
     const fetchGifs = async () => {
         try {
-            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${API_KEY}&q=${keyword.split(" ").join("")}&limit=1`);
-            const { data } = await response.json();
-            setGifUrl(data[0]?.images?.downsized_meduim?.url)
+            const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&q=${keyword.split(" ").join("")}&limit=1`);
+            const { data } = await response.json();     
+            setGifUrl(data[0]?.images?.downsized_medium.url);
         } catch (error) {
             setGifUrl('https://www.omnisend.com/blog/wp-content/uploads/2016/09/funny-gifs-9.gif')
         }
