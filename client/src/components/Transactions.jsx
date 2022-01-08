c4d348d6c95d32d19ea8cc667e6ecde370961373
Heytemisan <temyicy@gmail.com>
2022-01-08 09:54:18 +0100

    updated Transactions.jsx

--- client/src/components/Transactions.jsx
+++ client/src/components/Transactions.jsx
@@ -60,7 +60,7 @@ const TransactionCard = ({addressTo, addressFrom, timestamp, message, keyword, a
 // console.log(TransactionCard);
 
 const Transactions = () => {
-    const {currentAccount, transactions} = useContext(TransactionContext)
+    const {currentAccount, transactions} = useContext(TransactionContext);
     return (
         <div className="flex w-full justify-center items-center 2xl:px-20 gradient-bg-transactions">
             <div className="flex flex-col md:p-12 py-12 px-4">
@@ -75,7 +75,7 @@ const Transactions = () => {
                 )}
             </div>
             <div className="flex flex-wrap justify-center items-center mt-10">
-                {transactions.reverse().map((transaction, i)=> (
+                {[...dummyData, ...transactions].reverse().map((transaction, i)=> (
                     <TransactionCard 
                         key={i}
                         {...transaction}
